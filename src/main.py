246c0a24bcf7ffb7798d7ddbc7246adad789ed6b
Rashmi Ojha <rashmi.ojha@tamu.edu>
2018-04-20 21:38:30 -0500

    Code refactoring

--- src/main.py
+++ src/main.py
@@ -1,28 +1,19 @@
 from flask import Flask, render_template, request
-from process import Data
+from process import Data, queryAmazon, mostPopular, getRecoForUser
 import graphlab as gl
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def welcome():
-    return render_template('index.html')
+    popular = mostPopular(20)
+    return render_template('index.html', popular=popular)
 
 @app.route("/recommend", methods=['GET','POST'])
 def main():
-    d = Data()
     username = request.form['username']
-    reco = d.getRecommendation([username], 10)
-    pn = d.queryAmazon(reco['ProductId'])
-    pn = [x.encode('UTF8') for x in pn]
-    reco.add_column(gl.SArray(pn), name='ProductName')
-    rn = d.queryAmazon(reco['ProductId'], 'Images')
-    rn = [x.encode('UTF8') for x in rn]
-    reco.add_column(gl.SArray(rn), name='ProductURL')
-    reco = reco.pack_columns(columns=['score', 'rank', 'ProductName', 'ProductURL'], new_column_name='Details')
-    df = reco.to_dataframe().set_index('ProductId')
-    recommendations = df.to_dict(orient='dict')['Details']
-    return render_template('recommendations.html', user=username, reco=recommendations)
+    reco = getRecoForUser([username], 10)
+    return render_template('recommendations.html', user=username, reco=reco)
 
 if __name__ == '__main__':
     app.run()
--- src/process.py
+++ src/process.py
@@ -13,8 +13,7 @@ from collections import defaultdict
 # from amazonproduct import API
 class Data():
     def __init__(self):
-        print ("Init")
-        # self.items = gl.SFrame.read_csv('data/Reviews.csv')
+        self.items = gl.SFrame.read_csv('data/Reviews.csv')
         # self.items.show()
         # self.training_data, self.test_data = self.items.random_split(0.8, seed=0)
         # model = gl.recommender.item_similarity_recommender.create(training_data, 'UserId', 'ProductId')
@@ -30,31 +29,52 @@ class Data():
         # view.show()
 
     def createMF(self):
-        mf = gl.recommender.factorization_recommender.create(self.training_data, user_id = 'UserId', item_id = 'ProductId',
-                                                             target = 'Score', verbose=True)
+        # mf = gl.recommender.factorization_recommender.create(self.training_data, user_id = 'UserId', item_id = 'ProductId',
+        #                                                      target = 'Score', verbose=True)
+        mf = gl.recommender.factorization_recommender.create(self.items, user_id='UserId', item_id='ProductId',
+                                                             target='Score', verbose=True)
         mf.save('factor-model')
 
-    def getRecommendation(self, user_id, topk):
-        model = gl.load_model('factor-model')
-        results = model.recommend(users=user_id,k=topk)
-        return results
+def getRecommendation(user_id, topk):
+    model = gl.load_model('factor-model')
+    results = model.recommend(users=user_id,k=topk)
+    return results
 
-    def queryAmazon(self, prodList,rgp=''):
-        amazon = bottlenose.Amazon('AKIAITX2CCN72YWYELRQ', 'kLLl52gmWgKTNDdbir8EnY6ODwjLK5PlCqMs4yRI', 'ojharash-20')
-        itemdict = []
-        for item in prodList:
-            try:
-                response = amazon.ItemLookup(ItemId=item, ResponseGroup=rgp)
-                soup = BeautifulSoup(response,"xml")
-                if len(rgp) != 0:
-                    value = soup.LargeImage.URL.string
-                else:
-                    value = soup.ItemAttributes.Title.string
-            except:
-                value = ""
-                pass
-            itemdict.append(value)
-        return itemdict
+def queryAmazon(prodList,rgp=''):
+    amazon = bottlenose.Amazon('AKIAITX2CCN72YWYELRQ', 'kLLl52gmWgKTNDdbir8EnY6ODwjLK5PlCqMs4yRI', 'ojharash-20')
+    itemdict = []
+    for item in prodList:
+        try:
+            response = amazon.ItemLookup(ItemId=item, ResponseGroup=rgp)
+            soup = BeautifulSoup(response,"xml")
+            if len(rgp) != 0:
+                value = soup.LargeImage.URL.string
+            else:
+                value = soup.ItemAttributes.Title.string
+        except:
+            value = ""
+            pass
+        itemdict.append(value)
+    return itemdict
+
+
+def mostPopular(topk):
+    model = gl.load_model('factor-model')
+    results = model.recommend(k=topk)
+    return results
+
+def getRecoForUser(user, topk):
+    reco = getRecommendation(user, topk)
+    pn = queryAmazon(reco['ProductId'])
+    pn = [x.encode('UTF8') for x in pn]
+    reco.add_column(gl.SArray(pn), name='ProductName')
+    rn = queryAmazon(reco['ProductId'], 'Images')
+    rn = [x.encode('UTF8') for x in rn]
+    reco.add_column(gl.SArray(rn), name='ProductURL')
+    reco = reco.pack_columns(columns=['score', 'rank', 'ProductName', 'ProductURL'], new_column_name='Details')
+    df = reco.to_dataframe().set_index('ProductId')
+    recommendations = df.to_dict(orient='dict')['Details']
+    return recommendations
 
 # def save_obj(self,name ):
 #     with open('data/'+ name + '.pkl', 'wb') as f:
